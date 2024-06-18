6faee7cfb885a0d5df60af070041f8d894a248c2
Bueno <bueno.mateus@aluno.ifsp.edu.br>
2024-06-18 00:15:03 -0300

    chore: Add column 'visible' to table 'sugestoes' in bd.sql

--- bd.sql
+++ bd.sql
@@ -8,6 +8,15 @@ CREATE TABLE feedback (
     modificado_por VARCHAR (65), 
     ultima_modificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+CREATE TABLE sugestoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(65) NOT NULL,
+    email VARCHAR(125) NOT NULL,
+    conteudo TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
 ----teste----
 UPDATE feedback
 SET visible = FALSE,
